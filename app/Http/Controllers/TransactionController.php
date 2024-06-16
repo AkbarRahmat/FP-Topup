e0f01112f446f89a0d29d825d86e13216c9e2b74
brustcraft458 <brustcraft458@gmail.com>
2024-06-16 21:08:03 +0700

    fix

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,8 +9,9 @@ use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
-    public function getAllTransactionsGameTotal($status)//$status = null
+    public function getAllTransactionsGameTotal($status)
     {
+        // Query
         $transactions = DB::table('transactions');
         $transactions->join('users', 'transactions.user_id', '=', 'users.id');
         $transactions->join('products', 'transactions.product_id', '=', 'products.id');
@@ -24,12 +25,14 @@ class TransactionController extends Controller
             DB::raw('COUNT(transactions.user_id) as user_total')
         );
 
-        // Add condition to filter by status if provided
+        // Filter Status
         if ($status && $status != 'all') {
             $transactions->where('transactions.status', $status);
         }
 
         $transactions->groupBy('transactions.status', 'products.game_id', 'games.name');
+
+        // Data Result
         $result = $transactions->get();
 
         if ($result->isEmpty()) {
@@ -49,6 +52,7 @@ class TransactionController extends Controller
 
     public function getUserTransactionsByGame($status, $game_target)
     {
+        // Query
         $transactions = DB::table('transactions');
         $transactions->join('users', 'transactions.user_id', '=', 'users.id');
         $transactions->join('products', 'transactions.product_id', '=', 'products.id');
@@ -56,11 +60,19 @@ class TransactionController extends Controller
         $transactions->join('payments', 'transactions.payment_id', '=', 'payments.id');
         $transactions->select('products.name as product_name', 'games.name as game_name', 'products.price as product_price', 'payments.total_price as payment_price','transactions.usergame_name','transactions.usergame_id','transactions.usergame_server');
 
+        // Check UUID
         if (isUUID($game_target)) {
             $transactions->where('products.game_id', $game_target);
         } else {
             $transactions->where('games.name', $game_target);
         }
+
+        // Filter Status
+        if ($status && $status != 'all') {
+            $transactions->where('transactions.status', $status);
+        }
+
+        // Data Result
         $result = $transactions->get();
 
         if ($result->isEmpty()) {
@@ -76,10 +88,6 @@ class TransactionController extends Controller
             'message' => 'succes_get_trasaction_ingame_user',
             'data' => $result
         ]);
-
-        // 'success' => true,
-        // 'message' => 'Pesan berhasil mendapatkan transaksi di dalam game user',
-        // 'data' => $game_id
     }
 
 
